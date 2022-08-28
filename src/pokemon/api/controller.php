f56c990a84d7fdfae6fc928cdc1a943adb36633a
Jonatan Fransson <jonte.frasse@gmail.com>
2022-08-28 18:25:36 +0200

    Pokemons endpoint

--- src/pokemon/api/controller.php
+++ src/pokemon/api/controller.php
@@ -1,46 +1,63 @@
 <?php
+
+declare(strict_types=1);
+
 namespace pokemon\api;
 
-class controller extends \api\resource\controller {
-    public const FIELDS = [ 'PokemonID' => 'pokemon_id',
-                            'Description' => 'description',
-                            'Name' => 'name',
-                            'Shiny' => 'shiny',
-                            'Shadow' => 'shadow',
-                            'Galar' => 'galar',
-                            'Alolan' => 'alolan',
-                            'BaseAttack' => 'base_attack',
-                            'BaseDefense' => 'base_defense',
-                            'BaseStamina' => 'base_stamina',
-                            'PokemonTypeID1' => 'pokemon_type_id_1',
-                            'PokemonTypeID2' => 'pokemon_type_id_2' ];
-              
-    public function pokemon_list() {
+class controller extends \api\resource\controller
+{
+    public const FIELDS = [
+        'PokemonID' => 'pokemon_id',
+        'Description' => 'description',
+        'Name' => 'name',
+        'Shiny' => 'shiny',
+        'Shadow' => 'shadow',
+        'Galar' => 'galar',
+        'Alolan' => 'alolan',
+        'BaseAttack' => 'base_attack',
+        'BaseDefense' => 'base_defense',
+        'BaseStamina' => 'base_stamina',
+        'PokemonTypeID1' => 'pokemon_type_id_1',
+        'PokemonTypeID2' => 'pokemon_type_id_2'
+    ];
+
+    public function pokemon_list()
+    {
         $fields = array_keys(static::FIELDS);
-        $pokemon_data = $this->_resource_model->get_list($fields);        
+        $pokemon_data = model::list($fields);
         if (!$pokemon_data)
             throw new \Exception('Pokemon(s) not found', 404);
 
-        return self::format_response($pokemon_data);
+        return self::response($pokemon_data);
     }
 
-    public function pokemon_list_checksum() {
+    public function pokemon_list_checksum()
+    {
         $fields = array_keys(static::FIELDS);
-        $pokemon_data = $this->_resource_model->get_list($fields);
-        return [ md5(serialize($pokemon_data)) ];
+        $pokemon_data = model::list($fields);
+        return [
+            md5(serialize($pokemon_data))
+        ];
     }
-    
-    public function format_response(array $pokemon_data) {
+
+    public function response(array $pokemon_data): array
+    {
         $response = [];
         foreach ($pokemon_data as $pokemon)
             $response[] = self::format_pokemon($pokemon);
         return $response;
     }
 
-    public function format_pokemon(array $pokemon) {
+    public function format_pokemon(array $pokemon)
+    {
         $formated_pokemon = [];
         foreach (static::FIELDS as $db_field => $value_field)
             $formated_pokemon[$value_field] = $pokemon[$db_field];
         return $formated_pokemon;
+    }
+
+    public function validate(array $response): bool
+    {
+        return true;
     }
 }
