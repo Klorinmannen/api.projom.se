9adf3a5623b6f8719d23e19d20c8234d19937c8c
Klorinmannen <jonte.frasse@gmail.com>
2022-09-11 22:38:41 +0200

    sys/config

--- src/system/config.php
+++ src/system/config.php
@@ -12,8 +12,22 @@ class config
     private $_db = [];
     private $_api = [];
 
+    private $_system_dir = '';
+    private $_config_dir = '';
+    private $_source_dir = '';
+
     public function __construct()
     {
+        $system_dir = str_replace(
+            'src' . DIRECTORY_SEPARATOR . 'system',
+            '',
+            __DIR__
+        );
+
+        $this->_system_dir = realpath($system_dir);
+        $this->_config_dir = realpath($system_dir . '/config');
+        $this->_source_dir = realpath($system_dir . '/src');
+
         $this->_load_config();
         $this->_load_db_config();
         $this->_load_api_config();
@@ -21,21 +35,21 @@ class config
 
     private function _load_config(): void
     {
-        $filepath = str_replace('src\system', '', __DIR__) . 'config\system.json';
+        $filepath = $this->_config_dir . DIRECTORY_SEPARATOR . 'system.json';
         if (file_exists($filepath))
             $this->_config = json::parse($filepath);
     }
 
     private function _load_db_config(): void
     {
-        $filepath = $this->_config['config_dir'] . 'db.json';
+        $filepath = $this->_config_dir . DIRECTORY_SEPARATOR . 'db.json';
         if (file_exists($filepath))
             $this->_db = json::parse($filepath);
     }
 
     private function _load_api_config(): void
     {
-        $filepath = $this->_config['config_dir'] . 'api.json';
+        $filepath = $this->_config_dir . DIRECTORY_SEPARATOR . 'api.json';
         if (file_exists($filepath))
             $this->_api = json::parse($filepath);
     }
@@ -52,16 +66,21 @@ class config
 
     public function source_dir(): string
     {
-        return $this->_config['source_dir'];
+        return $this->_source_dir;
     }
 
     public function system_dir(): string
     {
-        return $this->_config['system_dir'];
+        return $this->_system_dir;
     }
 
     public function config_dir(): string
     {
-        return $this->_config['config_dir'];
+        return $this->_config_dir;
+    }
+
+    public function config(string $key)
+    {
+        return $this->_config[$key] ?? '';
     }
 }
