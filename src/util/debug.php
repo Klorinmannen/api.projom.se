c3e69f323834b3cdfcd92cd58701f0d1b14a5479
Klorinmannen <jonte.frasse@gmail.com>
2022-09-11 22:39:06 +0200

    added \util\debug::cml

--- src/util/debug.php
+++ src/util/debug.php
@@ -1,28 +1,43 @@
 <?php
+
+declare(strict_types=1);
+
 namespace util;
 
-class debug {
-    public static function sql(string $sql, string $lb = "\n") {
-        $output = preg_split('/(SELECT|FROM|WHERE|INSERT INTO|VALUES|INNER JOIN|ON|LEFT JOIN|RIGHT JOIN)/',
-                             $sql,
-                             -1,
-                             \PREG_SPLIT_NO_EMPTY|\PREG_SPLIT_DELIM_CAPTURE);
+class debug
+{
+    public static function sql(string $sql, string $lb = "\n")
+    {
+        $output = preg_split(
+            '/(SELECT|FROM|WHERE|INSERT INTO|VALUES|INNER JOIN|ON|LEFT JOIN|RIGHT JOIN)/',
+            $sql,
+            -1,
+            \PREG_SPLIT_NO_EMPTY | \PREG_SPLIT_DELIM_CAPTURE
+        );
         return implode($lb, $output);
     }
 
-    public static function as_string(array $subject) {
+    public static function as_string(array $subject)
+    {
         return var_export($subject);
     }
 
-    public static function html($subject) {
+    public static function html($subject)
+    {
         echo '<pre>';
         print_r($subject);
         echo '</pre>';
     }
 
-    public static function dump($subject) {
+    public static function dump($subject)
+    {
         echo '<pre>';
         var_dump($subject);
-        echo '</pre>';        
+        echo '</pre>';
+    }
+
+    public static function cml($subject)
+    {
+        print_r($subject);
     }
 }
